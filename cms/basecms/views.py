6e93eca8919f42f1e7a4e53e1d44a4fffadf3d18
M. Brandon Lorenz <coding.solo@gmail.com>
2011-08-14 20:51:09 -0400

    First commit

---
+++ cms/basecms/views.py
@@ -0,0 +1,5 @@
+# Create your views here.
+from django.shortcuts import HttpResponse
+
+def home(request):
+    return HttpResponse('hello!')
---
+++ cms/urls.py
@@ -0,0 +1,30 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'cms.views.home', name='home'),
+    # url(r'^cms/', include('cms.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    # Examples:
+    url(r'^$', 'basecms.views.home', name='home'),
+    # url(r'^cms/', include('cms.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^tiny_mce/(?P<path>.*)$','django.views.static.serve',
+        { 'document_root': '/virtualenvs/cms/tinymce/jscripts/tiny_mce/'}),
+    url(r'',include('django.contrib.flatpages.urls')),
+
+)

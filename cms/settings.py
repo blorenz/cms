03e4dabe036e35381b9c02cc9299114335009f7c
M. Brandon Lorenz <coding.solo@gmail.com>
2011-08-17 00:00:38 -0400

    Added search

--- cms/settings.py
+++ cms/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/virtualenvs/cms/cms/cms.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/root/workspaces/cmsimp/cms/src/cms/cms.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
--- cms/urls.py
+++ cms/urls.py
@@ -25,6 +25,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^tiny_mce/(?P<path>.*)$','django.views.static.serve',
         { 'document_root': '/virtualenvs/cms/tinymce/jscripts/tiny_mce/'}),
+                       
+    url(r"^search/$","search.views.search"),
     url(r'',include('django.contrib.flatpages.urls')),
 
 )

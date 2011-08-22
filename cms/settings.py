54850da2fb6f6e6b4574da17c064456ee3cd4f77
M. Brandon Lorenz <coding.solo@gmail.com>
2011-08-21 23:38:52 -0400

    DB update

--- cms/settings.py
+++ cms/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/root/workspaces/cmsimp/cms/src/cms/cms.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/virtualenvs/cms/cms/cms.db',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
